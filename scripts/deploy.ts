43c9ec48932c7cd9c65a1fc30cdc5ad84b02d9b8
Oleksandr Z <cyvubino@protonmail.com>
2021-11-22 23:19:55 +0200

    verification added to deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -15,11 +15,17 @@ async function main() {
 
   // We get the contract to deploy
   const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const greeting = "Hello, Hardhat!";
+  const greeter = await Greeter.deploy(greeting);
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  await new Promise(resolve => setTimeout(resolve, 60000)); // pause 3-4 blocks for etherscan update
+
+  // Verifying contract
+  await run("verify:verify", {address: greeter.address, constructorArguments: [greeting]});
+
+  console.log("Greeter deployed and verified to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
